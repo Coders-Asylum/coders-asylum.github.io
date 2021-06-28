4822669e108d4b9a96ad35edfacc1b1ed909e101
MavericK099 <adithyashetty032@gmail.com>
2021-06-28 23:34:37 +0530

    added new Scaffold wrapper

--- test/tools/WidgetTestTool.dart
+++ test/tools/WidgetTestTool.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 
 class WidgetTestTool {
   /// Returns the widget passed in the [child] after wrapping it with [MediaQuery] and [MaterialApp].
-  Widget scaffoldTestWidget(Widget child) {
+  Widget materialTestWidget(Widget child) {
     return MediaQuery(data: MediaQueryData(size: Size(1440, 1080)), child: MaterialApp(home: child));
+  }
+
+  ///  /// Returns the widget passed in the [child] after wrapping it with [MediaQuery] and [Scaffold].
+  Widget scaffoldTestWidget(Widget child) {
+    return materialTestWidget(Scaffold(body: child));
   }
 }
