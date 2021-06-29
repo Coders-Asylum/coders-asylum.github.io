23caadbafc50d6f047e3056dfd46416714e3ff6e
MavericK099 <adithyashetty032@gmail.com>
2021-06-30 01:37:31 +0530

    deployment scripts added

---
+++ .ci/deploy.sh
@@ -0,0 +1,6 @@
+git config --global user.name "codersAsylumDeployer"
+git config --global user.name "codersasylum@gmail.com"
+git remote set-url origin https://github.com/Coders-Asylum/coders-asylum.github.io.git
+
+git commit --all
+git push origin production 
