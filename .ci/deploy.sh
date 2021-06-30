75e75a934b4ffc0c487f703b7450e294b708da53
MavericK099 <adithyashetty032@gmail.com>
2021-06-30 21:24:07 +0530

    updated env variable name

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,6 +1,5 @@
-git config --global user.name "codersAsylumDeployer"
-git config --global user.name "codersasylum@gmail.com"
-git remote set-url origin https://$github_token@github.com/Coders-Asylum/coders-asylum.github.io.git
+git config --global user.name "Maverick099"
+git remote set-url origin https://$GITHUB_TOKEN@github.com/Coders-Asylum/coders-asylum.github.io.git
 
 git commit --all
 git push origin production 
