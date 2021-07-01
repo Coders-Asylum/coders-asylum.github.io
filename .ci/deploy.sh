8037e0fa4896528fc7283d6f723180aeb1da8c31
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2021-07-01 10:03:33 +0530

    Update deploy.sh

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,6 +1,6 @@
 
 git config --global user.name "Maverick099"
-# git remote set-url origin https://github.com/Coders-Asylum/coders-asylum.github.io.git
+git remote set-url origin https://${{ secrets.DEPLOY_TOKEN }}github.com/Coders-Asylum/coders-asylum.github.io.git
 
-git commit --all -m "New Build"
+git commit --all -m 'New Build'
 git push origin production 
