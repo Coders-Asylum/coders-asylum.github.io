870f19e2fada64a682e043998a25fb8ee0e2aae7
MavericK099 <adithyashetty032@gmail.com>
2021-07-01 07:53:15 +0530

    CHECK 7

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -2,5 +2,5 @@
 git config --global user.name "Maverick099"
 git remote set-url origin https://Maverick099:$GITHUB_TOKEN@github.com/Coders-Asylum/coders-asylum.github.io.git
 
-git commit --all -m "New Build"
+git commit --all -m 'New Build'
 git push origin production 
