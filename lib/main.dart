108c2840df31f96c3cd7fb36ee7eb5d8973616a1
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2021-09-19 20:11:22 +0530

    removed deprecated accent color defination

--- lib/main.dart
+++ lib/main.dart
@@ -9,16 +9,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    /// [appTheme] contains all the default theme colors for the application.
+    final ThemeData appTheme = ThemeData(
+      primaryColor: Color(0xffD3D3D3),
+      highlightColor: Color(0xff727272),
+      backgroundColor: Color(0xff1E1E1E),
+      shadowColor: Color(0xff000000),
+    );
+
     return MaterialApp(
       title: 'Flutter 101',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Color(0xffD3D3D3),
-        highlightColor: Color(0xff727272),
-        accentColor: Color(0xff1366C8),
-        backgroundColor: Color(0xff1E1E1E),
-        shadowColor: Color(0xff000000),
-      ),
+      theme: appTheme.copyWith(
+          colorScheme:
+              appTheme.colorScheme.copyWith(secondary: Color(0xff1366C8))),
       home: HomePage(),
     );
   }
