d3df0037d29f93d41ffc6c6b246a2c8dbb879164
Maverick099 <adithyashetty032@gmail.com>
2021-09-20 17:26:28 +0000

    Coverage report generated on:Mon 20 Sep 2021 05:26:28 PM UTC

--- test/coverage/coverage_hepler_test.dart
+++ test/coverage/coverage_hepler_test.dart
@@ -3,3 +3,23 @@
 // ignore_for_file: unused_import
 
 void main() {}
+/// Helper file to find coverge make coverage tests for all dart files.
+/// This file is created during every CI operation automaticaly, it is safe to delete.
+
+//ignore_for_file: unused_importimport 'package:web/main.dart';
+import 'package:web/pages/BlogPage.dart';
+import 'package:web/pages/HomePage.dart';
+import 'package:web/components/TextBlock.dart';
+import 'package:web/components/paint/TechLines.dart';
+import 'package:web/components/paint/ButtonAnimation.dart';
+import 'package:web/components/paint/CircleMatrix.dart';
+import 'package:web/components/MenuBar.dart';
+import 'package:web/components/Button.dart';
+import 'package:web/components/CodeBlock.dart';
+import 'package:web/components/Page.dart';
+import 'package:web/components/SideBar.dart';
+import 'package:web/components/NameIconWidget.dart';
+import 'package:web/components/blogPage/FeaturedPostTileComponents.dart';
+import 'package:web/components/blogPage/TopicsTileComponents.dart';
+
+void main(){}
