cd22a4b3bb4175ad48b04eb5bff480ec02193939
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2021-09-20 22:17:52 +0530

    fix: for auto generated dart file

--- .ci/coverage.sh
+++ .ci/coverage.sh
@@ -6,25 +6,26 @@ git remote set-url origin "https://${DEPLOY_TOKEN}@github.com/Coders-Asylum/code
 git fetch --all
 
 # creating the helper file.
-touch test/coverage/coverage_hepler_test.dart
-helper_file=test/coverage/coverage_hepler_test.dart
->helper_file
+if [[ ! -e ./test/coverage/coverage_hepler_test.dart ]]; then
+    mkdir -p ./test/coverage/
+    touch ./test/coverage/coverage_hepler_test.dart
+fi
+helper_file=./test/coverage/coverage_hepler_test.dart
+> helper_file
 
 printf "/// Helper file to find coverge make coverage tests for all dart files.\n" >> $helper_file
 printf "/// This file is created during every CI operation automaticaly, it is safe to delete.\n\n" >> $helper_file
-printf "// ignore_for_file: unused_import" >> $helper_file
+printf "//ignore_for_file: unused_import" >> $helper_file
 
 #  These lines find all the .dart file used in the project and adds them into the import lines with the package name.
 #  This ignores the .dart files created by flutter
 
 find lib '!' -path 'generated*/*' '!' -name '*.g.dart' '!' -name '*.part.dart' '!' -name '*.freezed.dart' -name '*.dart' | cut -c4- | awk -v package=web '{printf "import '\''package:%s%s'\'';\n", package, $1}' >> $helper_file
-echo "\nvoid main(){}" >> $helper_file
+printf "\nvoid main(){}\n" >> $helper_file
 
 flutter test --pub --null-assertions --coverage --coverage-path "test/coverage/report/lcov.info"
 
 # Runs only if PR has triggered the CI
-if [ -n "$CIRRUS_PR" ]; 
-then
   # check if changes are present in the current working branch
   if ! git --git-dir="./.git" diff --quiet
   then
@@ -33,7 +34,6 @@ then
     git push origin
     echo "Changes have been committed and pushed to origin"
   fi
-fi
 
 # This sends a dispatch event
 curl --location --request POST 'https://api.github.com/repos/Coders-Asylum/coders-asylum.github.io/dispatches' \
