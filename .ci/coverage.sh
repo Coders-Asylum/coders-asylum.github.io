4e929127f1fb6626dd65ff8375becd93acd7c516
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2021-09-20 19:16:23 +0530

    sh codequality fix1

--- .ci/coverage.sh
+++ .ci/coverage.sh
@@ -1,27 +1,23 @@
 #! /bin/sh
-
 echo "setting git configurations for commit."
 git config --global user.email "${EMAIL}"
 git config --global user.name "${USERNAME}"
-echo $USERNAME
 git remote set-url origin "https://${DEPLOY_TOKEN}@github.com/Coders-Asylum/coders-asylum.github.io.git"
 git fetch --all
 
-echo "installing curl"
-sudo apt-get install curl
-
-echo "Creating helper file"
+# creating the helper file.
 touch test/coverage/coverage_hepler_test.dart
 helper_file=test/coverage/coverage_hepler_test.dart
+>helper_file
 
-echo "/// Helper file to find coverge make coverage tests for all dart files.\n" > $helper_file
-echo "/// This file is created during every CI operation automaticaly, it is safe to delete.\n" > $helper_file
-echo "// ignore_for_file: unused_import" >> $helper_file
+printf "/// Helper file to find coverge make coverage tests for all dart files.\n" >> $helper_file
+printf "/// This file is created during every CI operation automaticaly, it is safe to delete.\n\n" >> $helper_file
+printf "// ignore_for_file: unused_import" >> $helper_file
 
 #  These lines find all the .dart file used in the project and adds them into the import lines with the package name.
 #  This ignores the .dart files created by flutter
 
-find lib '!' -path 'generated*/*' '!' -name '*.g.dart' '!' -name '*.part.dart' '!' -name '*.freezed.dart' -name '*.dart' | cut -c4- | awk -v package=$1  package=$1 '{printf "import '\''package:%s%s'\'';\n", package, $1}' >> $helper_file
+find lib '!' -path 'generated*/*' '!' -name '*.g.dart' '!' -name '*.part.dart' '!' -name '*.freezed.dart' -name '*.dart' | cut -c4- | awk -v package=web '{printf "import '\''package:%s%s'\'';\n", package, $1}' >> $helper_file
 echo "\nvoid main(){}" >> $helper_file
 
 flutter test --pub --null-assertions --coverage --coverage-path "test/coverage/report/lcov.info"
