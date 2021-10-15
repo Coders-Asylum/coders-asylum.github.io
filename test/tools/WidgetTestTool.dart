8eea3452e3e161df1b908b9987d28c6f77601bed
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2021-10-15 19:32:34 +0530

    added new tool MaterialApp

--- test/tools/WidgetTestTool.dart
+++ test/tools/WidgetTestTool.dart
@@ -2,12 +2,20 @@ import 'package:flutter/material.dart';
 
 class WidgetTestTool {
   /// Returns the widget passed in the [child] after wrapping it with [MediaQuery] and [MaterialApp].
-  Widget materialTestWidget(Widget child) {
+  Widget mediaQueryTestWidget(Widget child) {
     return MediaQuery(data: MediaQueryData(size: Size(1440, 1080)), child: MaterialApp(home: child));
   }
 
   ///  /// Returns the widget passed in the [child] after wrapping it with [MediaQuery] and [Scaffold].
   Widget scaffoldTestWidget(Widget child) {
-    return materialTestWidget(Scaffold(body: child));
+    return mediaQueryTestWidget(Scaffold(body: child));
+  }
+
+  /// Material app to test larger multifunctional widgets and pages.
+  ///
+  MaterialApp buildTestApp(Widget child){
+    return MaterialApp(
+      home: scaffoldTestWidget(child),
+    );
   }
 }
