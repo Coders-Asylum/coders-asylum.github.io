2986636197cdb7f8b72de1274e76f4b49f9eb6ce
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2021-11-08 21:51:20 +0530

    Update WidgetTestTool.dart
    added option to have a custom screen-size while testing.

--- test/tools/WidgetTestTool.dart
+++ test/tools/WidgetTestTool.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
 
 class WidgetTestTool {
-  /// Returns the widget passed in the [child] after wrapping it with [MediaQuery] and [MaterialApp].
   /// [screenSize] is used to specify a custom screen size to test the app in.
+  Size _screenSize= Size(1440, 1080);
+
+  /// Screen size for the current testing environment.
+  get screenSize  => this._screenSize;
+
+
+  /// Returns the widget passed in the [child] after wrapping it with [MediaQuery] and [MaterialApp].
   Widget mediaQueryTestWidget(Widget child, {Size screenSize = const Size(1440, 1080)}) {
+    this._screenSize = screenSize;
     return MediaQuery(data: MediaQueryData(size: screenSize), child: MaterialApp(home: child));
   }
 
   ///  /// Returns the widget passed in the [child] after wrapping it with [MediaQuery] and [Scaffold].
-  Widget scaffoldTestWidget(Widget child) {
-    return mediaQueryTestWidget(Scaffold(body: child));
+  Widget scaffoldTestWidget(Widget child, {Size screenSize = const Size(1440, 1080)}) {
+    return mediaQueryTestWidget(Scaffold(body: child),screenSize: screenSize);
   }
 
   /// Material app to test larger multifunctional widgets and pages.
   ///
-  MaterialApp buildTestApp(Widget child) {
+  MaterialApp buildTestApp(Widget child, {Size screenSize = const Size(1440, 1080)}) {
     return MaterialApp(
-      home: scaffoldTestWidget(child),
+      home: scaffoldTestWidget(child,screenSize: screenSize),
     );
   }
 }
