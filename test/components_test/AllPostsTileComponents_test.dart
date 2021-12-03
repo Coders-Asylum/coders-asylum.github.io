dc984d5edd66a26d4ed6793bd5c82031dd8254aa
Maverick099 <adithyashetty032@gmail.com>
2021-12-03 15:44:57 +0000

    Formatting Changes done on Fri 03 Dec 2021 03:44:57 PM UTC

--- test/components_test/AllPostsTileComponents_test.dart
+++ test/components_test/AllPostsTileComponents_test.dart
@@ -24,7 +24,7 @@ void main() {
       await tester.pumpAndSettle();
 
       expect(find.byWidget(_build), findsOneWidget);
-    },skip: true);
+    }, skip: true);
   });
 
   group('ImagePost test', () {
